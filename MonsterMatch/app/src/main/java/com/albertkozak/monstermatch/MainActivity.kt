66a397b2fb6081491d50f474d34b83061a91eb06
Albert Kozak <albert.kozak@live.com>
2020-02-06 18:59:50 -0800

    Refactored code to use 1 list instead of 3 in MainActivity

--- MonsterMatch/app/src/main/java/com/albertkozak/monstermatch/MainActivity.kt
+++ MonsterMatch/app/src/main/java/com/albertkozak/monstermatch/MainActivity.kt
@@ -5,20 +5,11 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val heads: List<Int> = listOf(
-        R.drawable.monster1_head,
-        R.drawable.monster2_head,
-        R.drawable.monster3_head)
-
-    private val bodies: List<Int> = listOf(
-        R.drawable.monster1_body,
-        R.drawable.monster2_body,
-        R.drawable.monster3_body)
-
-    private val feet: List<Int> = listOf(
-        R.drawable.monster1_feet,
-        R.drawable.monster2_feet,
-        R.drawable.monster3_feet)
+    private val monsters: List<Monster> = listOf(
+        Monster(R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
+        Monster(R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
+        Monster(R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +20,14 @@ class MainActivity : AppCompatActivity() {
         match_button.setOnClickListener {
             shuffleMonsters()
         }
-//        monster_head_image_view.setImageResource(R.drawable.monster1_head)
     }
 
     private fun shuffleMonsters()
     {
-        monster_head_image_view.setImageResource(heads.random())
-        monster_body_image_view.setImageResource(bodies.random())
-        monster_feet_image_view.setImageResource(feet.random())
+        monster_head_image_view.setImageResource(monsters.random().head)
+        monster_body_image_view.setImageResource(monsters.random().body)
+        monster_feet_image_view.setImageResource(monsters.random().feet)
     }
 }
+
+data class Monster(val head: Int, val body: Int, val feet: Int)
