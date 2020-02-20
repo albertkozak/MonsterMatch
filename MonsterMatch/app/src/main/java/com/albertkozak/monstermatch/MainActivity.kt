01730667e0088cbfb13a840b45d79905d70e5272
Albert Kozak <albert.kozak@live.com>
2020-02-19 19:03:56 -0800

    Added successful match banner

--- MonsterMatch/app/src/main/java/com/albertkozak/monstermatch/MainActivity.kt
+++ MonsterMatch/app/src/main/java/com/albertkozak/monstermatch/MainActivity.kt
@@ -6,9 +6,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val monsters: List<Monster> = listOf(
-        Monster(R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
-        Monster(R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
-        Monster(R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
+        Monster("Bobo", R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
+        Monster("BingBing", R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
+        Monster("Goopi", R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +22,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun shuffleMonsters()
-    {
-        monster_head_image_view.setImageResource(monsters.random().head)
-        monster_body_image_view.setImageResource(monsters.random().body)
-        monster_feet_image_view.setImageResource(monsters.random().feet)
+    private fun shuffleMonsters(){
+        val theHead:Int=monsters.random().head
+        monster_head_image_view.setImageResource(theHead)
+        monster_head_image_view.setTag(theHead)
+        val theBody:Int=monsters.random().body
+        monster_body_image_view.setImageResource(theBody)
+        monster_body_image_view.setTag(theBody)
+        val theFeet:Int=monsters.random().feet
+        monster_feet_image_view.setImageResource(theFeet)
+        monster_feet_image_view.setTag(theFeet)
+
+        if(monster_head_image_view.getTag().toString() == R.drawable.monster1_head.toString() &&
+            monster_body_image_view.getTag().toString() == R.drawable.monster1_body.toString() &&
+            monster_feet_image_view.getTag().toString() == R.drawable.monster1_feet.toString())
+                {game_text.setText(monsters[0].name)}
+        else if(monster_head_image_view.getTag().toString() == R.drawable.monster2_head.toString() &&
+            monster_body_image_view.getTag().toString() == R.drawable.monster2_body.toString() &&
+            monster_feet_image_view.getTag().toString() == R.drawable.monster2_feet.toString())
+                {game_text.setText(monsters[1].name)}
+        else if(monster_head_image_view.getTag().toString() == R.drawable.monster3_head.toString() &&
+            monster_body_image_view.getTag().toString() == R.drawable.monster3_body.toString() &&
+            monster_feet_image_view.getTag().toString() == R.drawable.monster3_feet.toString())
+                {game_text.setText(monsters[2].name)}
+        else {game_text.setText("")}
     }
 }
 
-data class Monster(val head: Int, val body: Int, val feet: Int)
+data class Monster(val name: String, val head: Int, val body: Int, val feet:Int)
